636d0f62276d2f4e2ab7a58c6e3ef6dd980366c9
AndrewD14 <Leingod2005@gmail.com>
2017-10-10 21:48:16 -0500

    Added in the function to check answers, new function to go to the next question, global constants, global variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
 //global variables
 var mainTimer;
 var questionList;
+var questionsRight;
+var questionsWrong;
+var questionsCounter;
+
+//global constants
+var WAITTOSHOWANSWER = 5000;
+var WAITFORSELECTION = 5000;
+var WAITFORNEXTQUESTION = 3000;
 
 //function to get new questions
 function getQuestions(){
@@ -22,6 +30,11 @@ function getQuestions(){
 		success: populateQuestion,
 		error: populateQuestionError
 	});
+
+	//resets the various global variables
+	questionsRight = 0;
+	questionsWrong = 0;
+	questionsCounter = 0;
 }
 
 //function to parse the response JSON
@@ -29,6 +42,7 @@ function populateQuestion(response){
 	console.log(response.results);
 	questionList = response.results;
 
+	$("#start").css("display", "block");
 	$("#start").click(startGame);
 }
 
@@ -39,23 +53,107 @@ function populateQuestionError(response){
 
 //function to start the timer for the game
 function startGame(){
-	$("#question").html(questionList[0].question);
-	mainTimer = setTimeout(displayAnswers, 5000, 0);
+	$("#start").css("display", "none");
+	
+	displayQuestion(questionsCounter);
 }
 
-function displayAnswers(num){
-	for(i in questionList[num].incorrect_answers){
+//shows the current question
+function displayQuestion(){
+	//clears the question first
+	$("#question").empty();
+
+	//clears the possible answers first
+	$("#answers").empty();
+
+	$("#question").html(questionList[questionsCounter].question);
+
+	//sets a timer to wait to display the answers so the player has time to read first
+	mainTimer = setTimeout(displayAnswers, WAITTOSHOWANSWER);
+}
+
+//reveals the possible answers
+function displayAnswers(){
+	var alreadyDisplayed = [];
+
+	do{
+		//grabs a random index to select to show on the screen
+		var random;
+		var found
+
+		do{
+			random = Math.floor(Math.random() * (questionList[questionsCounter].incorrect_answers.length+1));
+
+			 found = false;
+
+			for(i in alreadyDisplayed){
+				if(alreadyDisplayed[i] == random)
+					found = true;
+			}
+		}while(found && alreadyDisplayed.length < 4)
+
 		var newDiv = $("<div>");
-		newDiv.html(questionList[num].incorrect_answers[i]);
+		newDiv.addClass("answers");
+
+		//determines if it is the correct answer to show or an incorrect one
+		if(random <= 2){
+			newDiv.html(questionList[questionsCounter].incorrect_answers[random]);
+			newDiv.attr("data", questionList[questionsCounter].incorrect_answers[random]);
+		}
+		else{
+			newDiv.html(questionList[questionsCounter].correct_answer);
+			newDiv.attr("data", questionList[questionsCounter].correct_answer);
+		}
+
+		newDiv.click(checkAnswer);
+		
 		$("#answers").append(newDiv);
+
+		//adds to the array of indexes of already displayed answers
+		alreadyDisplayed.push(random);
+	}while(alreadyDisplayed.length < 4);
+
+	$("#answers").append(newDiv);
+	mainTimer = setTimeout(ranOutOfTime, WAITFORSELECTION);
+}
+
+//function that forces the player to lose the guess if taking too long
+function ranOutOfTime(){
+	//increment wrong guesses
+	questionsWrong++;
+
+	getNextQuestion();
+}
+
+//function to grab the next question
+function getNextQuestion(){
+	//increment question counter by 1
+	if(questionsCounter < questionList.length-1){
+		questionsCounter++;
+
+		//sets a timer to wait to display the next question
+		mainTimer = setTimeout(displayQuestion, WAITFORNEXTQUESTION);
+	}
+}
+
+//function to check for correct answer
+function checkAnswer(){
+	//clears the timer for waiting for a guess
+	clearTimeout(mainTimer);
+
+	//removes the click to not allow players to guess multiple times
+	$("#answers .answers").unbind("click");
+
+	//checks the attr with the correct answer
+	if($(this).attr("data") == questionList[questionsCounter].correct_answer){
+		questionsRight++;
+		console.log("You got it right");
+	}
+	else{
+		questionsWrong++;
+		console.log("You got it wrong");
 	}
 
-	var newDiv = $("<div>");
-	newDiv.html(questionList[num].correct_answer);
-	$("#answers").append(newDiv);
-	mainTimer = setTimeout(ranOutOfTime, 3000);
-}
-
-function ranOutOfTime(){
-	console.log("You ran out of time");
+	//calls the function to queue the next question
+	getNextQuestion();
 }
